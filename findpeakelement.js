2edd970ff4d61ecff017304e3517d3d30724a3b6
kevindai777 <kevin.dai.2451@gmail.com>
2020-06-12 21:57:20 -0400

    solutions

---
+++ findpeakelement.js
@@ -0,0 +1,36 @@
+//Objective is to find the index of a peak element in a 1-D array, or an element that is greater
+//than its neighbors.
+
+let nums = [1,2,3,1]
+
+
+//O(n) approach that does a linear scan and finds an element that is greater than its neighbor.
+
+//Case 1: Rising slope (will return last element since loop is never satisfied)
+//Case 2: Descending slope (will return first element since first element satisfies loop)
+//Case 3: Mountain slope (will return mountain peak due to loop)
+
+for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > nums[i + 1]) {
+        return i
+    }
+}
+
+return nums.length - 1
+
+
+//O(logn) approach that performs a binary search and returns the 'low' as the peak
+
+let low = 0
+let high = nums.length - 1
+
+while (low < high) {
+    let mid = Math.floor((low + high)/2)
+    if (nums[mid] > nums[mid + 1]) {
+        high = mid
+    } else {
+        low = mid + 1
+    }
+}
+
+return low
